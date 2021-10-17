9d196a38df1a6abebd499d8ea16ae298bea2ef25
XPFO <90360434+XPFO@users.noreply.github.com>
2021-10-16 22:27:41 -1000

    Update site-config.js
    Update site-config.js

--- src/gatsby-ipfs-web-wallet/components/site-config.js
+++ src/gatsby-ipfs-web-wallet/components/site-config.js
@@ -4,8 +4,8 @@
 */
 
 const config = {
-  title: 'Wallet Demo',
-  titleShort: 'Demo',
+  title: 'MY BCH WALLET 2.0.1',
+  titleShort: 'A FORK BCH WALLET',
   balanceText: 'BCH Balance',
   balanceIcon: 'fab-bitcoin',
 
@@ -13,12 +13,6 @@ const config = {
   // hash of the mirror of this site.
   memoAddr: 'bitcoincash:qzr4s6fwk55zvl7xqsjpr4k7zkqfmvcz7qapxys8t5',
 
-  // Footer Information
-  hostText: 'FullStack.cash',
-  hostUrl: 'https://fullstack.cash/',
-  sourceCode: 'https://github.com/Permissionless-Software-Foundation/bch-wallet-starter',
-  torUrl: 'tozqhxcrknbv5l3wshwgj6vp3bj5ezw7qp4jdn4bjrhcpetpcq74j7ad.onion',
-  clearWebUrl: 'https://demo-wallet.fullstack.cash'
-}
+  }
 
 module.exports = config
